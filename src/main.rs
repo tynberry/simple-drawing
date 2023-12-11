c68aa384473b13e08af63c30bd8ae1d649c9f2d8
Tynberry <robotm@email.cz>
2023-12-11 10:23:42 +0100

    default egui,macroquad combo

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,25 @@
-#[macroquad::main("Paint")]
+use macroquad::prelude::*;
+
+#[macroquad::main("egui with macroquad")]
 async fn main() {
-    println!("Hello, world!");
+    loop {
+        clear_background(WHITE);
+
+        // Process keys, mouse etc.
+
+        egui_macroquad::ui(|egui_ctx| {
+            egui::Window::new("egui ❤ macroquad")
+                .show(egui_ctx, |ui| {
+                    ui.label("Test");
+                });
+        });
+
+        // Draw things before egui
+
+        egui_macroquad::draw();
+        
+        // Draw things after egui
+
+        next_frame().await;
+    }
 }
