3872fc839255fcfed5fe544510f522372c9b2152
Tynberry <robotm@email.cz>
2023-12-11 10:46:18 +0100

    added actual rendering

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use macroquad::prelude::*;
 #[macroquad::main("egui with macroquad")]
 async fn main() {
     //YES
-    let mut point_a: Vec2 = Vec2::new(0.0, 0.0);
-    let mut s_1: f64 = 0.0;
-    let mut s_2: f64 = 0.0;
-    let mut ang: f64 = 0.0;
+    let mut point_a: Vec2 = Vec2::new(182.0, 500.0);
+    let mut s_1: f64 = 420.0;
+    let mut s_2: f64 = 420.0;
+    let mut ang: f64 = -69.0 * std::f64::consts::PI / 180.0;
     let mut thicc: f32 = 5.0;
 
     loop {
@@ -53,10 +53,41 @@ async fn main() {
         egui_macroquad::draw();
 
         //vypočti třetí stranu 
-        let s_3 = s_1.powi(2) + s_2.powi(2) + s_1 * s_2 * ang.cos();
+        //let s_3 = s_1.powi(2) + s_2.powi(2) + s_1 * s_2 * ang.cos();
 
         //vykresli trojúhelník
-        
+        let point_b = point_a + Vec2::new(1.0, 0.0) * s_1 as f32;
+        let point_c = point_a + Vec2::from_angle(ang as f32).rotate(Vec2::new(1.0, 0.0)) * s_2 as f32;
+
+        draw_line(
+            point_a.x,
+            point_a.y,
+            point_b.x,
+            point_b.y,
+            thicc, 
+            BLACK
+        );        
+        draw_line(
+            point_b.x,
+            point_b.y,
+            point_c.x,
+            point_c.y,
+            thicc, 
+            BLACK
+        );        
+        draw_line(
+            point_a.x,
+            point_a.y,
+            point_c.x,
+            point_c.y,
+            thicc, 
+            BLACK
+        );        
+
+        draw_circle(point_a.x, point_a.y, thicc / 2.0, BLACK);
+        draw_circle(point_b.x, point_b.y, thicc / 2.0, BLACK);
+        draw_circle(point_c.x, point_c.y, thicc / 2.0, BLACK);
+
 
         next_frame().await;
     }
